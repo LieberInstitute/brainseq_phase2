feae9024a70a332e8296c027530973e7213d30c9
lcolladotor <lcollado@jhsph.edu>
2018-02-22 10:03:54 -0500

    Version control recent modifications made by others

--- caseControl_analysis_DLPFC.R
+++ caseControl_analysis_DLPFC.R
@@ -6,32 +6,34 @@ library(sva)
 library(edgeR)
 library(limma)
 
-## load gene
-load("count_data/dlpfc_ribozero_brainseq_phase2_hg38_rseGene_merged_n449.rda")
-colData(rse_gene)$RIN = sapply(colData(rse_gene)$RIN,"[",1)
-colData(rse_gene)$totalAssignedGene = sapply(colData(rse_gene)$totalAssignedGene,"[",1)
-colData(rse_gene)$mitoRate = sapply(colData(rse_gene)$mitoRate,"[",1)
-colData(rse_gene)$overallMapRate = sapply(colData(rse_gene)$overallMapRate,"[",1)
-colData(rse_gene)$rRNA_rate = sapply(colData(rse_gene)$rRNA_rate,"[",1)
-colData(rse_gene)$ERCCsumLogErr = sapply(colData(rse_gene)$ERCCsumLogErr,"[",1)
-colData(rse_gene)$Kit = ifelse(colData(rse_gene)$mitoRate < 0.05, "Gold", "HMR")
-getRPKM = recount::getRPKM
+## load expression data
+load("/dcl01/lieber/ajaffe/lab/brainseq_phase2/expr_cutoff/rse_gene.Rdata")
+load("/dcl01/lieber/ajaffe/lab/brainseq_phase2/expr_cutoff/rse_exon.Rdata")
+load("/dcl01/lieber/ajaffe/lab/brainseq_phase2/expr_cutoff/rse_jxn.Rdata")
+load("/dcl01/lieber/ajaffe/lab/brainseq_phase2/expr_cutoff/rse_tx.Rdata")
 
-## load qSVs
-load("count_data/degradation_rse_phase2_dlpfc.rda")
+colData(rse_gene)$RIN = sapply(colData(rse_gene)$RIN,"[",1)
+colData(rse_gene)$totalAssignedGene = sapply(colData(rse_gene)$totalAssignedGene, mean)
+colData(rse_gene)$mitoRate = sapply(colData(rse_gene)$mitoRate,mean)
+colData(rse_gene)$overallMapRate = sapply(colData(rse_gene)$overallMapRate, mean)
+colData(rse_gene)$rRNA_rate = sapply(colData(rse_gene)$rRNA_rate,mean)
+colData(rse_gene)$ERCCsumLogErr = sapply(colData(rse_gene)$ERCCsumLogErr,mean)
+colData(rse_gene)$Kit = ifelse(colData(rse_gene)$mitoRate < 0.05, "Gold", "HMR")
 
 ## filter for age and race
 keepIndex = which(rse_gene$Age > 17 & 
-	rse_gene$Kit == "Gold" & rse_gene$Race %in% c("AA", "CAUC"))
+	rse_gene$Kit == "Gold" & rse_gene$Race %in% c("AA", "CAUC") & 
+	rse_gene$Region == "DLPFC")
 rse_gene = rse_gene[,keepIndex]
-cov_rse_dlpfc = cov_rse_dlpfc[,keepIndex]
+rse_exon = rse_exon[,keepIndex]
+rse_jxn = rse_jxn[,keepIndex]
+rse_tx = rse_tx[,keepIndex]
 
-## filter low expression
-geneIndex = which(rowMeans(getRPKM(rse_gene,"Length")) > 0.2)
-rse_gene = rse_gene[geneIndex,]
 
-## Ns
-table(rse_gene$Dx)
+##################
+## load qSVs
+load("count_data/degradation_rse_phase2_dlpfc.rda")
+cov_rse_dlpfc = cov_rse_dlpfc[,sapply(rse_gene$SAMPLE_ID, "[", 1)]
 
 ## add mds data
 mds = read.table("/dcl01/lieber/ajaffe/lab/brainseq_phase2/genotype_data/BrainSeq_Phase2_RiboZero_Genotypes_n546_maf05_geno10_hwe1e6.mds",
@@ -53,21 +55,60 @@ qSVs = pcaDeg$x[,1:k]
 getPcaVars(pcaDeg)[1:k]
 modQsva = cbind(mod, qSVs)
 
-## modeling
+###################
+## modeling #######
+
+##### GENE ######
 dge = DGEList(counts = assays(rse_gene)$counts, 
 	genes = rowData(rse_gene))
 dge = calcNormFactors(dge)
-
-## mean-variance
 vGene = voom(dge,modQsva, plot=TRUE)
-
-## do analysis
 fitGene = lmFit(vGene)
 ebGene = ebayes(fitGene)
-
-## top table
 eBGene = eBayes(fitGene)
 sigGene = topTable(eBGene,coef=2,
 	p.value = 1,number=nrow(rse_gene))
 outGene = sigGene[rownames(rse_gene),]
-save(outGene, file = "caseControl/dxStats_dlpfc_filtered_qSVA.rda")
+
+## no qSVA
+vGene0 = voom(dge,mod, plot=TRUE)
+fitGene0 = lmFit(vGene0)
+eBGene0 = eBayes(fitGene0)
+sigGene0 = topTable(eBGene0,coef=2,
+	p.value = 1,number=nrow(rse_gene))
+outGene0 = sigGene0[rownames(rse_gene),]
+save(outGene, outGene0, file = "caseControl/dxStats_DLPFC_filtered_qSVA_geneLevel.rda")
+
+##### Exon ######
+dee = DGEList(counts = assays(rse_exon)$counts, 
+	genes = rowData(rse_exon))
+dee = calcNormFactors(dee)
+vExon = voom(dee,modQsva, plot=TRUE)
+fitExon = lmFit(vExon)
+ebExon = ebayes(fitExon)
+eBExon = eBayes(fitExon)
+sigExon = topTable(eBExon,coef=2,
+	p.value = 1,number=nrow(rse_exon))
+outExon = sigExon[rownames(rse_exon),]
+
+##### Junction ######
+dje = DGEList(counts = assays(rse_jxn)$counts, 
+	genes = rowData(rse_jxn))
+dje = calcNormFactors(dje)
+vJxn = voom(dje,modQsva, plot=TRUE)
+fitJxn = lmFit(vJxn)
+ebJxn = ebayes(fitJxn)
+eBJxn = eBayes(fitJxn)
+sigJxn = topTable(eBJxn,coef=2,
+	p.value = 1,number=nrow(rse_jxn))
+outJxn = sigJxn[rownames(rse_jxn),]
+
+##### Transcript ######
+fitTx = lmFit(assays(rse_tx)$tpm, modQsva)
+ebTx = ebayes(fitTx)
+eBTx = eBayes(fitTx)
+sigTx = topTable(eBTx,coef=2,
+	p.value = 1,number=nrow(rse_tx))
+outTx = sigTx[rownames(rse_tx),]
+save(outGene, outExon, outJxn,outTx,
+	file = "caseControl/dxStats_DLPFC_filtered_qSVA.rda")
