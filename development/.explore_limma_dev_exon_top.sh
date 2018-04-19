5b400e22a7704bb183a1301ac6c3fd1168d43ad6
lcolladotor <lcollado@jhsph.edu>
2018-04-19 18:01:36 -0400

    Update top plots for dev and region specific models

---
+++ development/.explore_limma_dev_exon_top.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#$ -cwd
+#$ -l bluejay,mem_free=60G,h_vmem=60G,h_fsize=100G
+#$ -N explore_limma_dev_exon_top
+#$ -o ./logs/explore_limma_dev_exon_top.txt
+#$ -e ./logs/explore_limma_dev_exon_top.txt
+#$ -m e
+
+echo "**** Job starts ****"
+date
+
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${JOB_ID}"
+echo "Job name: ${JOB_NAME}"
+echo "Hostname: ${HOSTNAME}"
+echo "Task id: ${TASK_ID}"
+
+module load conda_R/3.4.x
+Rscript explore_limma_dev_top.R -t exon
+
+echo "**** Job ends ****"
+date
