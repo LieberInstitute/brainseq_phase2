65fae4eec6750b0c9b8062e4e01504277a962f59
Leonardo Collado Torres <lcollado@jhsph.edu>
2018-08-08 13:47:34 -0400

    Start adapting @emilyburke's eQTL code for GTEx. First just copied it

---
+++ eQTL_full_GTEx/eQTL_interaction.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#$ -cwd
+#$ -l bluejay,mem_free=200G,h_vmem=200G,h_fsize=200G
+#$ -N inter_eQTL_bsII
+#$ -o eqtl_tables/logs/eQTL_interaction_4_features2.txt
+#$ -e eqtl_tables/logs/eQTL_interaction_4_features2.txt
+#$ -m a
+echo "**** Job starts ****"
+date
+
+Rscript run_eqtls_interaction.R
+
+echo "**** Job ends ****"
+date
