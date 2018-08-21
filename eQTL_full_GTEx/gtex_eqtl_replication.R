f22bacde6142f7d9c2d3a1411ca90f502e6bd16c
lcolladotor <lcollado@jhsph.edu>
2018-08-21 16:13:43 -0400

    Ran chmod

--- eQTL_full_GTEx/gtex_eqtl_replication.R
+++ eQTL_full_GTEx/gtex_eqtl_replication.R
@@ -6,9 +6,9 @@ library(MatrixEQTL)
 library(sva)
 
 load("eqtl_tables/mergedEqtl_output_dlpfc_4features.rda", verbose=TRUE)
-dlpfc = allEqtl
+dlpfc_gtex = allEqtl
 load("eqtl_tables/mergedEqtl_output_hippo_4features.rda")
-hippo = allEqtl
+hippo_gtex = allEqtl
 rm(allEqtl)
 
 
