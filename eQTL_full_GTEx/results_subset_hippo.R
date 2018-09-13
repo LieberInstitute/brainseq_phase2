ce7373c707b0302f99e2e893ad5e0eeef4b51393
lcolladotor <lcollado@jhsph.edu>
2018-09-13 13:50:39 -0400

    Try using data.table for subsetting based on https://stackoverflow.com/questions/27303534/faster-way-to-subset-on-rows-of-a-data-frame-in-r and data.table vignettes

--- eQTL_full_GTEx/results_subset_hippo.R
+++ eQTL_full_GTEx/results_subset_hippo.R
@@ -16,56 +16,64 @@ library('devtools')
 # GTEx
 message(paste(Sys.time(), 'loading GTEx eQTL results'))
 load("eqtl_tables/mergedEqtl_output_hippo_4features.rda", verbose=TRUE)
+
+message(paste(Sys.time(), 'checking for NAs on the GTEx eQTL table'))
+na_vec <- !is.na(allEqtl$snps) & !is.na(allEqtl$gene)
+table(na_vec)
+if(any(!na_vec)) {
+    message(paste(Sys.time(), 'removing NAs from the GTEx eQTL table'))
+    allEqtl <- allEqtl[na_vec, ]
+}
+
+message(paste(Sys.time(), 'convert to a data.table'))
+allEqtl <- data.table(as.data.frame(allEqtl))
+
 # break up into pieces
-message(paste(Sys.time(), 'breaking up into pieces'))
-hippo_gtex_genes = allEqtl[which(allEqtl$Type=="Gene"),]
-hippo_gtex_exons = allEqtl[which(allEqtl$Type=="Exon"),]
-hippo_gtex_jxns = allEqtl[which(allEqtl$Type=="Jxn"),]
-hippo_gtex_txs = allEqtl[which(allEqtl$Type=="Tx"),]
+message(paste(Sys.time(), 'breaking up by feature'))
+hippo_gtex_genes = allEqtl[allEqtl$Type=="Gene",]
+hippo_gtex_exons = allEqtl[allEqtl$Type=="Exon",]
+hippo_gtex_jxns = allEqtl[allEqtl$Type=="Jxn",]
+hippo_gtex_txs = allEqtl[allEqtl$Type=="Tx",]
 rm(allEqtl)
-
-message(paste(Sys.time(), 'setting row names'))
-rownames(hippo_gtex_genes) = paste0(hippo_gtex_genes$snps,"_",hippo_gtex_genes$gene)
-rownames(hippo_gtex_exons) = paste0(hippo_gtex_exons$snps,"_",hippo_gtex_exons$gene)
-rownames(hippo_gtex_jxns) = paste0(hippo_gtex_jxns$snps,"_",hippo_gtex_jxns$gene)
-rownames(hippo_gtex_txs) = paste0(hippo_gtex_txs$snps,"_",hippo_gtex_txs$gene)
-
 
 # BrainSeq
 message(paste(Sys.time(), 'loading BrainSeq Phase II eQTL results'))
 load("../eQTL_full/eqtl_tables/mergedEqtl_output_hippo_4features.rda", verbose=TRUE)
+
 # keep only significant
+message(paste(Sys.time(), 'subsetting to significant results'))
 h_sig = allEqtl[allEqtl$FDR < 0.01,]
 rm(allEqtl)
 
-message(paste(Sys.time(), 'setting row names'))
-rownames(h_sig) = paste0(h_sig$snps,"_",h_sig$gene)
-
-message(paste(Sys.time(), 'subsetting to significant results'))
-h_sig_genes = h_sig[which(h_sig$Type=="Gene"),]
-h_sig_exons = h_sig[which(h_sig$Type=="Exon"),]
-h_sig_jxns = h_sig[which(h_sig$Type=="Jxn"),]
-h_sig_txs = h_sig[which(h_sig$Type=="Tx"),]
+message(paste(Sys.time(), 'breaking up by feature')
+h_sig_genes = h_sig[h_sig$Type=="Gene",]
+h_sig_exons = h_sig[h_sig$Type=="Exon",]
+h_sig_jxns = h_sig[h_sig$Type=="Jxn",]
+h_sig_txs = h_sig[h_sig$Type=="Tx",]
 rm(h_sig)
 
 ## subset GTEx to our results
+subset_gtex <- function(gtex, brainseq) {
+    gtex[gtex$snps == brainseq$snps & gtex$gene == brainseq$gene, ]
+}
+
 message(paste(Sys.time(), 'matching gene results'))
-hippo_gtex_genes = hippo_gtex_genes[rownames(i_sig_genes),]
+hippo_gtex_genes <- subset_gtex(hippo_gtex_genes, h_sig_genes)
 message(paste(Sys.time(), 'saving gene results'))
 save(hippo_gtex_genes,h_sig_genes, "hippo_compare_genes.rda")
 
 message(paste(Sys.time(), 'matching exon results'))
-hippo_gtex_exons = hippo_gtex_exons[rownames(i_sig_exons),]
+hippo_gtex_exons = hippo_gtex_exons[rownames(h_sig_exons),]
 message(paste(Sys.time(), 'saving exon results'))
 save(hippo_gtex_exons,h_sig_exons, "hippo_compare_exons.rda")
 
 message(paste(Sys.time(), 'matching jxn results'))
-hippo_gtex_jxns = hippo_gtex_jxns[rownames(i_sig_jxns),]
+hippo_gtex_jxns = hippo_gtex_jxns[rownames(h_sig_jxns),]
 message(paste(Sys.time(), 'saving jxn results'))
 save(hippo_gtex_jxns,h_sig_jxns, "hippo_compare_jxns.rda")
 
 message(paste(Sys.time(), 'matching tx results'))
-hippo_gtex_txs = hippo_gtex_txs[rownames(i_sig_txs),]
+hippo_gtex_txs = hippo_gtex_txs[rownames(h_sig_txs),]
 message(paste(Sys.time(), 'saving tx results'))
 save(hippo_gtex_txs,h_sig_txs, "hippo_compare_txs.rda")
 
--- eQTL_full_GTEx/results_subset_interaction.R
+++ eQTL_full_GTEx/results_subset_interaction.R
@@ -2,6 +2,7 @@
 ### libraries
 library(SummarizedExperiment)
 library(jaffelab)
+library('data.table')
 library('devtools')
 
 
@@ -19,58 +20,49 @@ library('devtools')
 # GTEx
 message(paste(Sys.time(), 'loading GTEx eQTL results'))
 load("eqtl_tables/mergedEqtl_output_interaction_4features.rda", verbose=TRUE)
+
+message(paste(Sys.time(), 'checking for NAs on the GTEx eQTL table'))
+na_vec <- !is.na(allEqtl$snps) & !is.na(allEqtl$gene)
+table(na_vec)
+if(any(!na_vec)) {
+    message(paste(Sys.time(), 'removing NAs from the GTEx eQTL table'))
+    allEqtl <- allEqtl[na_vec, ]
+}
+
+message(paste(Sys.time(), 'convert to a data.table'))
+allEqtl <- data.table(as.data.frame(allEqtl))
+
 # break up into pieces
-message(paste(Sys.time(), 'breaking up into pieces'))
-inter_gtex_genes = allEqtl[which(allEqtl$Type=="Gene"),]
-inter_gtex_exons = allEqtl[which(allEqtl$Type=="Exon"),]
-inter_gtex_jxns = allEqtl[which(allEqtl$Type=="Jxn"),]
-inter_gtex_txs = allEqtl[which(allEqtl$Type=="Tx"),]
+message(paste(Sys.time(), 'breaking up by feature'))
+inter_gtex_genes = allEqtl[allEqtl$Type=="Gene",]
+inter_gtex_exons = allEqtl[allEqtl$Type=="Exon",]
+inter_gtex_jxns = allEqtl[allEqtl$Type=="Jxn",]
+inter_gtex_txs = allEqtl[allEqtl$Type=="Tx",]
 rm(allEqtl)
-
-message(paste(Sys.time(), 'setting row names'))
-rownames(inter_gtex_genes) = paste0(inter_gtex_genes$snps,"_",inter_gtex_genes$gene)
-rownames(inter_gtex_exons) = paste0(inter_gtex_exons$snps,"_",inter_gtex_exons$gene)
-rownames(inter_gtex_jxns) = paste0(inter_gtex_jxns$snps,"_",inter_gtex_jxns$gene)
-## Remove NAs: otherwise it crashes due to duplicated names (there are just a handful)
-# > x <- paste0(inter_gtex_txs$snps,"_",inter_gtex_txs$gene)
-# > length(x)
-# [1] 50185567
-# > length(unique(x))
-# [1] 50185558
-# > y <- duplicated(x)
-# > sum(y)
-# [1] 9
-# > x[y]
-# [1] "NA_NA" "NA_NA" "NA_NA" "NA_NA" "NA_NA" "NA_NA" "NA_NA" "NA_NA" "NA_NA"
-message(paste(Sys.time(), 'removing NAs from the tx GTEx eQTL table'))
-table(!is.na(inter_gtex_txs$snps) & !is.na(inter_gtex_txs$gene))
-inter_gtex_txs <- inter_gtex_txs[!is.na(inter_gtex_txs$snps) & !is.na(inter_gtex_txs$gene), ]
-rownames(inter_gtex_txs) = paste0(inter_gtex_txs$snps,"_",inter_gtex_txs$gene)
-
 
 # BrainSeq
 message(paste(Sys.time(), 'loading BrainSeq Phase II eQTL results'))
 load("../eQTL_full/eqtl_tables/mergedEqtl_output_interaction_4features.rda", verbose=TRUE)
+
 # keep only significant
+message(paste(Sys.time(), 'subsetting to significant results'))
 i_sig = allEqtl[allEqtl$FDR < 0.01,]
 rm(allEqtl)
 
-message(paste(Sys.time(), 'setting row names'))
-rownames(i_sig) = paste0(i_sig$snps,"_",i_sig$gene)
-
-message(paste(Sys.time(), 'subsetting to significant results'))
-i_sig_genes = i_sig[which(i_sig$Type=="Gene"),]
-i_sig_exons = i_sig[which(i_sig$Type=="Exon"),]
-i_sig_jxns = i_sig[which(i_sig$Type=="Jxn"),]
-i_sig_txs = i_sig[which(i_sig$Type=="Tx"),]
+message(paste(Sys.time(), 'breaking up by feature'))
+i_sig_genes = i_sig[i_sig$Type=="Gene",]
+i_sig_exons = i_sig[i_sig$Type=="Exon",]
+i_sig_jxns = i_sig[i_sig$Type=="Jxn",]
+i_sig_txs = i_sig[i_sig$Type=="Tx",]
 rm(i_sig)
 
-system.time( test_1 <- inter_gtex_genes[rownames(i_sig_genes),] )
-system.time( test_2 <- inter_gtex_genes[inter_gtex_genes$snps == i_sig_genes$snps & inter_gtex_genes$gene == i_sig_genes$gene, ] ) 
-
 ## subset GTEx to our results
+subset_gtex <- function(gtex, brainseq) {
+    gtex[gtex$snps == brainseq$snps & gtex$gene == brainseq$gene, ]
+}
+
 message(paste(Sys.time(), 'matching gene results'))
-inter_gtex_genes = inter_gtex_genes[rownames(i_sig_genes),]
+inter_gtex_genes <- subset_gtex(inter_gtex_genes, i_sig_genes)
 message(paste(Sys.time(), 'saving gene results'))
 save(inter_gtex_genes,i_sig_genes, "inter_compare_genes.rda")
 
