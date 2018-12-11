38f6dd5d90d716c152fa7057940575fb4e7c3cf6
lcolladotor <lcollado@jhsph.edu>
2018-12-11 17:14:27 -0500

    More complicated code that uses plink but should ensure the subsetting works

--- twas/reference_hg38/port_to_hg38.R
+++ twas/reference_hg38/port_to_hg38.R
@@ -8,25 +8,20 @@ R
 
 ## Now run R code
 library('data.table')
-# library('SummarizedExperiment')
 library('sessioninfo')
 
+dir.create('LDREF_hg38')
+
 ## based on ../filter_data/filter_snps.R
-their_bims <- dir('LDREF_hg38', '.*bim$', full.names = TRUE)
-names(their_bims) <- dir('LDREF_hg38', '.*bim$')
+their_bims <- dir('/dcl01/lieber/ajaffe/lab/brainseq_phase2/twas/filter_data/LDREF', '.*bim$', full.names = TRUE)
+names(their_bims) <- dir('/dcl01/lieber/ajaffe/lab/brainseq_phase2/twas/filter_data/LDREF', '.*bim$')
 
 ldref_bim <- lapply(their_bims, function(input_bim) {
     message(paste(Sys.time(), 'reading file', input_bim))
-    original <- paste0(input_bim, '.original')
-    if(file.exists(original)) {
-        ## start from the original file if it's around
-        system(paste('mv', original, input_bim))
-    }
     res <- fread(input_bim,
         col.names = c('chr', 'snp', 'position', 'basepair', 'allele1', 'allele2'),
         colClasses = c('character', 'character', 'numeric', 'integer', 'character', 'character')
     )
-    system(paste('mv', input_bim, original))
     setkey(res, 'chr', 'basepair')
     return(res)
 })
@@ -44,17 +39,43 @@ setkey(snpMap, 'chr', 'basepair')
 mapply(function(ref_bim, bim_file, chr) {
     our <- snpMap[.(ref_bim$chr, ref_bim$basepair)]
     m <- !is.na(our$pos_hg38)
-    new <- ref_bim[m, ]
-    new$basepair <- our$basepair[m]
+    
+    ## Write the list of snps that do show up in our data
+    filt_snps <- paste0('1000G.EUR.', chr, '.snps.txt')
+    fwrite(ref_bim$snp[m],
+        file = filter_snps,
+        sep = '\t', col.names = FALSE
+    )   
+    
+    newbfile <- paste0('LDREF_hg38/1000G.EUR.', chr)
+    newbfile_bim <- paste0(newbfile, '.bim')
+    
+    message(paste(Sys.time(), 'subsetting the original LDREF files with plink'))
+    system(paste("plink --bfile", gsub('.bim', '', bim_file), '--extract', filt_snps,
+         "--make-bed --out", newbfile, " --memory 10000"))
+        
+    ## Read the new file
+    message(paste(Sys.time(), 'read the new bim file'))
+    final_bim <- fread(newbfile_bim,
+        col.names = c('chr', 'snp', 'position', 'basepair', 'allele1', 'allele2'),
+        colClasses = c('character', 'character', 'numeric', 'integer', 'character', 'character')
+    )
+    
+    ## Complete matching code later
+    m_final <- 
+    final_bim$basepair <- our$pos_hg38[m_final]
     
     message(paste(Sys.time(), 'Write new filtered bim file for chr', chr))
-    fwrite(new,
-        file = bim_file,
+    fwrite(
+        final_bim,
+        file = newbfile_bim,
         sep = '\t', col.names = FALSE
     )
-    return(file.exists(bim_file))
+
+    return(file.exists(newbfile_bim))
     
 }, ldref_bim, their_bims, gsub('1000G.EUR.|.bim', '', names(their_bims)))
+
 
 
 ## Reproducibility information
