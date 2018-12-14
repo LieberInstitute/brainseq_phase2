ffb04a72ad3a57a7b1ea6e5aead7f7e8dcd545f2
lcolladotor <lcollado@jhsph.edu>
2018-12-14 17:32:27 -0500

    Download PGC2 SCZ sumstats

---
+++ twas/pgc_scz2_sumstats/download.sh
@@ -0,0 +1,13 @@
+wget https://data.broadinstitute.org/alkesgroup/FUSION/SUM/PGC2.SCZ.sumstats
+# --2018-12-14 15:47:17--  https://data.broadinstitute.org/alkesgroup/FUSION/SUM/PGC2.SCZ.sumstats
+# Resolving data.broadinstitute.org... 69.173.92.29
+# Connecting to data.broadinstitute.org|69.173.92.29|:443... connected.
+# HTTP request sent, awaiting response... 200 OK
+# Length: 55730612 (53M)
+# Saving to: “PGC2.SCZ.sumstats”
+#
+# 100%[=============================================================================================================================================================================================================>] 55,730,612   863K/s   in 63s
+#
+# 2018-12-14 15:48:23 (865 KB/s) - “PGC2.SCZ.sumstats” saved [55730612/55730612]
+date
+# Fri Dec 14 15:48:24 EST 2018
