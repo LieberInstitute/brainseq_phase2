c12c9061eda8846ae026c89e5d4ee060c7dc9b37
lcolladotor <lcollado@jhsph.edu>
2019-01-24 10:03:44 -0500

    Save exploration/test code changes from yesterday

--- twas/test_analysis.R
+++ twas/test_analysis.R
@@ -1,23 +1,107 @@
 module load fusion_twas/github
 
-##
+## Choose chr
+chr="21"
+
+for chr in {1..22}
+do
+    echo "*************************"
+    echo ""
+    echo "processing chromosome ${chr}"
+    date
+    echo ""
+
+## Create summarized analysis
 Rscript /jhpce/shared/jhpce/libd/fusion_twas/github/fusion_twas/FUSION.assoc_test.R \
     --sumstats /dcl01/lieber/ajaffe/lab/brainseq_phase2/twas/pgc_scz2_sumstats/PGC2.SCZ.sumstats_hg38_ourname \
     --weights /dcl01/lieber/ajaffe/lab/brainseq_phase2/twas/HIPPO/gene/HIPPO_gene.pos \
     --weights_dir /dcl01/lieber/ajaffe/lab/brainseq_phase2/twas/HIPPO/gene \
     --ref_ld_chr /dcl01/lieber/ajaffe/lab/brainseq_phase2/twas/reference_hg38/LDREF_hg38/1000G.EUR. \
-    --chr 22 \
-    --out test_PGC2.SCZ.22.dat
+    --chr ${chr} \
+    --out test_PGC2.SCZ.${chr}.dat
     
+    echo ""
+    echo "making plots for chromosome ${chr}"
+    date
+    echo ""
+
 ## companion plotting step
 Rscript /jhpce/shared/jhpce/libd/fusion_twas/github/fusion_twas/FUSION.post_process.R \
     --sumstats /dcl01/lieber/ajaffe/lab/brainseq_phase2/twas/pgc_scz2_sumstats/PGC2.SCZ.sumstats_hg38_ourname \
-    --input test_PGC2.SCZ.22.dat \
-    --out test_PGC2.SCZ.22.analysis \
+    --input test_PGC2.SCZ.${chr}.dat \
+    --out test_PGC2.SCZ.${chr}.analysis \
     --ref_ld_chr /dcl01/lieber/ajaffe/lab/brainseq_phase2/twas/reference_hg38/LDREF_hg38/1000G.EUR. \
-    --chr 22 \
-    --plot --locus_win 100000 --verbose 2
+    --chr ${chr} \
+    --plot --locus_win 100000 --verbose 2 --plot_individual --plot_eqtl --plot_corr
+
+done
+
+library('readr')
+
+## Weird issue with chr 6
+info_all <- lapply(c(1:5, 7:22), function(chr) {
+    message(paste(Sys.time(), 'reading chromosome', chr))
+    patt <- paste0('test_PGC2.SCZ.', chr, '\\..*dat')
+    files <- dir(pattern = patt)
+    if(length(files) == 0) {
+        warning(paste('no files for chromosome', chr))
+        return(NULL)
+    }
+    info <- lapply(files, read_tsv)
+    names(info) <- gsub(paste0('test_PGC2.SCZ.', chr, '.|analysis.'), '', dir(pattern = patt))
+
+    # table(is.na(info[['dat']]$EQTL.ID))
+    # sapply(info, dim)
     
+    # chr <- 5
+    # ids <- unlist(sapply(info[grepl('joint', names(info))], function(x) x$ID))
+    # non_na_id <- info[['dat']]$ID[!is.na(info[['dat']]$EQTL.ID)]
+    # s(subset(info[['dat']], ID %in% non_na_id[which(!non_na_id %in% ids)]))
+    # non_na_id[133]
+
+    stopifnot(sum(sapply(info[grepl('joint', names(info))], nrow)) == sum(!is.na(info[['dat']]$EQTL.ID)) - sum(info[['dat']]$TWAS.P == 1, na.rm = TRUE))
+    return(info)
+})
+names(info_all) <- c(1:5, 7:22)
+
+info_all <- info_all[!sapply(info_all, is.null)]
+
+
+library('purrr')
+
+info_all <- purrr::map(purrr::transpose(info_all), function(x) do.call(rbind, x))
+## Missing chrs: 3, 6 and 13
+which(!1:22 %in% unique(info_all[['dat']]$CHR))
+
+library('GenomicRanges')
+load('/dcl01/lieber/ajaffe/lab/brainseq_phase2/eQTL_GWAS_riskSNPs/eqtl_tables/mergedEqtl_output_hippo_raggr_4features.rda', verbose = TRUE)
+
+
+dim(allEqtl)
+summary(allEqtl$FDR)
+
+eGene <- subset(allEqtl, Type == 'Gene' & FDR < 0.01)
+length(unique(eGene$gene))
+
+table(info_all[['joint_dropped.dat']]$ID %in% unique(eGene$gene))
+table(info_all[['joint_included.dat']]$ID %in% unique(eGene$gene))
+table(info_all[['dat']]$ID %in% unique(eGene$gene))
+
+
+table(info_all[['joint_dropped.dat']]$ID %in% unique(allEqtl$gene))
+table(info_all[['joint_included.dat']]$ID %in% unique(allEqtl$gene))
+table(info_all[['dat']]$ID %in% unique(allEqtl$gene))
+
+
+summary(info_all[['joint_included.dat']])
+summary(subset(info_all[['joint_included.dat']], ID %in% unique(eGene$gene)))
+
+x <- subset(info_all[['joint_included.dat']], ID %in% unique(eGene$gene))
+x
+y <- subset(info_all[['dat']], ID %in% x$ID)
+as.data.frame(y)
+
+as.data.frame(y)[which.min(y$TWAS.P), ]
 
 ## Original goal
 Rscript /jhpce/shared/jhpce/libd/fusion_twas/github/fusion_twas/FUSION.assoc_test.R \
