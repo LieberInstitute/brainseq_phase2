1e833dde1294f23820a69d1f1fb266c2736c3509
lcolladotor <lcollado@jhsph.edu>
2019-02-28 14:03:26 -0500

    Adapt code so it can resume under qsub. Will probably take a while to start running due to JHPCE's current workload

--- bsp1/eqtl/full/run_eqtls_dlpfc.R
+++ bsp1/eqtl/full/run_eqtls_dlpfc.R
@@ -1,10 +1,10 @@
 ####
 
 ### libraries
-library(SummarizedExperiment)
-library(jaffelab)
-library(MatrixEQTL)
-library(sva)
+library('SummarizedExperiment')
+library('jaffelab')
+library('MatrixEQTL')
+library('sva')
 library('sessioninfo')
 
 ######################
@@ -36,183 +36,171 @@ rse_tx = bsp1_tx[,keepInd]
 rm(bsp1_gene, bsp1_exon, bsp1_jxn, bsp1_tx)
 table(rse_gene$Dx)
 
-## extract pd and rpkms
-pd = colData(rse_gene)
-geneRpkm = assays(rse_gene)$rpkm
-exonRpkm = assays(rse_exon)$rpkm
-jxnRp10m = assays(rse_jxn)$rp10m
-txTpm = assays(rse_tx)$tpm
+
+if(!file.exists('eqtl_tables/matrixEqtl_output_dlpfc_4features.rda')) {
+    ## extract pd and rpkms
+    pd = colData(rse_gene)
+    geneRpkm = assays(rse_gene)$rpkm
+    exonRpkm = assays(rse_exon)$rpkm
+    jxnRp10m = assays(rse_jxn)$rp10m
+    txTpm = assays(rse_tx)$tpm
 
 
-######################
-### snp data ####
-######################
+    ######################
+    ### snp data ####
+    ######################
 
-## load SNP data
-load('/dcl01/ajaffe/data/lab/brainseq_phase1/genotype_data/brainseq_phase1_Genotypes_n732.rda', verbose = TRUE)
+    ## load SNP data
+    load('/dcl01/ajaffe/data/lab/brainseq_phase1/genotype_data/brainseq_phase1_Genotypes_n732.rda', verbose = TRUE)
 
-### make mds and snp dimensions equal to N
-###(repeat rows or columns for BrNum replicates)
-mds = mds[pd$BrNum,]
-snp = snp[,pd$BrNum]
-rownames(mds) = colnames(snp) = pd$RNum
-
-
-## drop SNPs not mapping to hg38
-keepIndex = which(!is.na(snpMap$chr_hg38))
-snpMap = snpMap[keepIndex,]
-snp = snp[keepIndex,]
+    ### make mds and snp dimensions equal to N
+    ###(repeat rows or columns for BrNum replicates)
+    mds = mds[pd$BrNum,]
+    snp = snp[,pd$BrNum]
+    rownames(mds) = colnames(snp) = pd$RNum
 
 
-######################
-# statistical model ##
-######################
-
-pd$Dx = factor(pd$Dx,
-	levels = c("Control", "Schizo", "MDD", "Bipolar"))
-
-mod = model.matrix(~Dx + Sex + as.matrix(mds[,1:5]),
-	data = pd)
-colnames(mod)[grep('mds', colnames(mod))] = colnames(mds)[1:5]
+    ## drop SNPs not mapping to hg38
+    keepIndex = which(!is.na(snpMap$chr_hg38))
+    snpMap = snpMap[keepIndex,]
+    snp = snp[keepIndex,]
 
 
-######################
-# create SNP objects #
-######################
+    ######################
+    # statistical model ##
+    ######################
 
-theSnps = SlicedData$new(as.matrix(snp))
-theSnps$ResliceCombined(sliceSize = 50000)
+    pd$Dx = factor(pd$Dx,
+    	levels = c("Control", "Schizo", "MDD", "Bipolar"))
 
-snpspos = snpMap[,c("SNP","chr_hg38","pos_hg38")]
-colnames(snpspos) = c("name","chr","pos")
-
-
-#######################
-####### do PCA ########
-#######################
-
-pcaGene = prcomp(t(log2(geneRpkm+1)))
-kGene = num.sv(log2(geneRpkm+1), mod)
-genePCs = pcaGene$x[,1:kGene]
-
-pcaExon = prcomp(t(log2(exonRpkm+1)))
-kExon = num.sv(log2(exonRpkm+1), mod, vfilter=50000)
-exonPCs = pcaExon$x[,1:kExon]
-
-pcaJxn = prcomp(t(log2(jxnRp10m+1)))
-kJxn = num.sv(log2(jxnRp10m+1), mod, vfilter=50000)
-jxnPCs = pcaJxn$x[,1:kJxn]
-
-pcaTx = prcomp(t(log2(txTpm+1)))
-kTx = num.sv(log2(txTpm+1), mod, vfilter=50000)
-txPCs = pcaTx$x[,1:kTx]
-
-save(genePCs, exonPCs, jxnPCs, txPCs, 
-	file="eqtl_tables/rdas/pcs_dlpfc_4features_filtered_over13.rda")
-# load("eqtl_tables/rdas/pcs_dlpfc_4features_filtered_over13.rda")
-
-covsGene = SlicedData$new(t(cbind(mod[,-1],genePCs)))
-covsExon = SlicedData$new(t(cbind(mod[,-1],exonPCs)))
-covsJxn = SlicedData$new(t(cbind(mod[,-1],jxnPCs)))
-covsTx = SlicedData$new(t(cbind(mod[,-1],txPCs)))
-
-##########################
-### feature annotation ###
-##########################
-
-###### gene level
-posGene = as.data.frame(rowRanges(rse_gene))[,1:3]
-posGene$name = rownames(posGene)
-posGene = posGene[,c(4,1:3)]
-
-##### exon level 
-posExon = as.data.frame(rowRanges(rse_exon))[,1:3]
-posExon$name = rownames(posExon)
-posExon = posExon[,c(4,1:3)]
-
-##### junction level 
-posJxn = as.data.frame(rowRanges(rse_jxn))[,1:3]
-posJxn$name = rownames(posJxn)
-posJxn = posJxn[,c(4,1:3)]
-names(posJxn)[2:4] = c("Chr", "Start","End")
-
-##### transcript level 
-posTx = as.data.frame(rowRanges(rse_tx))[,1:3]
-posTx$name = rownames(posTx)
-posTx = posTx[,c(4,1:3)]
-names(posTx)[2:4] = c("Chr", "Start","End")
+    mod = model.matrix(~Dx + Sex + as.matrix(mds[,1:5]),
+    	data = pd)
+    colnames(mod)[grep('mds', colnames(mod))] = colnames(mds)[1:5]
 
 
-#############################
-### sliced expression data ##
-geneSlice = SlicedData$new(log2(geneRpkm+1))
-exonSlice = SlicedData$new(log2(exonRpkm+1))
-jxnSlice = SlicedData$new(log2(jxnRp10m+1))
-txSlice = SlicedData$new(log2(txTpm+1))
+    ######################
+    # create SNP objects #
+    ######################
 
-geneSlice$ResliceCombined(sliceSize = 5000)
-exonSlice$ResliceCombined(sliceSize = 5000)
-jxnSlice$ResliceCombined(sliceSize = 5000)
-txSlice$ResliceCombined(sliceSize = 5000)
+    theSnps = SlicedData$new(as.matrix(snp))
+    theSnps$ResliceCombined(sliceSize = 50000)
+
+    snpspos = snpMap[,c("SNP","chr_hg38","pos_hg38")]
+    colnames(snpspos) = c("name","chr","pos")
 
 
-##########################
-### Run EQTLs ############
-##########################
-print("Begin eQTL analysis")
+    #######################
+    ####### do PCA ########
+    #######################
 
-meGene = Matrix_eQTL_main(snps=theSnps, gene = geneSlice, 
-	cvrt = covsGene, output_file_name.cis =  ".ctxt" ,
-	pvOutputThreshold.cis = 1,  pvOutputThreshold=0,
-	snpspos = snpspos, genepos = posGene, 
-	useModel = modelLINEAR,	cisDist=5e5,
-	pvalue.hist = 100,min.pv.by.genesnp = TRUE)	
+    pcaGene = prcomp(t(log2(geneRpkm+1)))
+    kGene = num.sv(log2(geneRpkm+1), mod)
+    genePCs = pcaGene$x[,1:kGene]
 
-meExon = Matrix_eQTL_main(snps=theSnps, gene = exonSlice, 
-	cvrt = covsExon, output_file_name.cis =  ".ctxt" ,
-	pvOutputThreshold.cis = 1,  pvOutputThreshold=0,
-	snpspos = snpspos, genepos = posExon, 
-	useModel = modelLINEAR,	cisDist=5e5,
-	pvalue.hist = 100,min.pv.by.genesnp = TRUE)	
+    pcaExon = prcomp(t(log2(exonRpkm+1)))
+    kExon = num.sv(log2(exonRpkm+1), mod, vfilter=50000)
+    exonPCs = pcaExon$x[,1:kExon]
 
-meJxn = Matrix_eQTL_main(snps=theSnps, gene = jxnSlice, 
-	cvrt = covsJxn, output_file_name.cis =  ".ctxt" ,
-	pvOutputThreshold.cis = 1,  pvOutputThreshold=0,
-	snpspos = snpspos, genepos = posJxn, 
-	useModel = modelLINEAR,	cisDist=5e5,
-	pvalue.hist = 100,min.pv.by.genesnp = TRUE)	
+    pcaJxn = prcomp(t(log2(jxnRp10m+1)))
+    kJxn = num.sv(log2(jxnRp10m+1), mod, vfilter=50000)
+    jxnPCs = pcaJxn$x[,1:kJxn]
+
+    pcaTx = prcomp(t(log2(txTpm+1)))
+    kTx = num.sv(log2(txTpm+1), mod, vfilter=50000)
+    txPCs = pcaTx$x[,1:kTx]
+
+    save(genePCs, exonPCs, jxnPCs, txPCs, 
+    	file="eqtl_tables/rdas/pcs_dlpfc_4features_filtered_over13.rda")
+    # load("eqtl_tables/rdas/pcs_dlpfc_4features_filtered_over13.rda")
+
+    covsGene = SlicedData$new(t(cbind(mod[,-1],genePCs)))
+    covsExon = SlicedData$new(t(cbind(mod[,-1],exonPCs)))
+    covsJxn = SlicedData$new(t(cbind(mod[,-1],jxnPCs)))
+    covsTx = SlicedData$new(t(cbind(mod[,-1],txPCs)))
+
+    ##########################
+    ### feature annotation ###
+    ##########################
+
+    ###### gene level
+    posGene = as.data.frame(rowRanges(rse_gene))[,1:3]
+    posGene$name = rownames(posGene)
+    posGene = posGene[,c(4,1:3)]
+
+    ##### exon level 
+    posExon = as.data.frame(rowRanges(rse_exon))[,1:3]
+    posExon$name = rownames(posExon)
+    posExon = posExon[,c(4,1:3)]
+
+    ##### junction level 
+    posJxn = as.data.frame(rowRanges(rse_jxn))[,1:3]
+    posJxn$name = rownames(posJxn)
+    posJxn = posJxn[,c(4,1:3)]
+    names(posJxn)[2:4] = c("Chr", "Start","End")
+
+    ##### transcript level 
+    posTx = as.data.frame(rowRanges(rse_tx))[,1:3]
+    posTx$name = rownames(posTx)
+    posTx = posTx[,c(4,1:3)]
+    names(posTx)[2:4] = c("Chr", "Start","End")
+
+
+    #############################
+    ### sliced expression data ##
+    geneSlice = SlicedData$new(log2(geneRpkm+1))
+    exonSlice = SlicedData$new(log2(exonRpkm+1))
+    jxnSlice = SlicedData$new(log2(jxnRp10m+1))
+    txSlice = SlicedData$new(log2(txTpm+1))
+
+    geneSlice$ResliceCombined(sliceSize = 5000)
+    exonSlice$ResliceCombined(sliceSize = 5000)
+    jxnSlice$ResliceCombined(sliceSize = 5000)
+    txSlice$ResliceCombined(sliceSize = 5000)
+
+
+    ##########################
+    ### Run EQTLs ############
+    ##########################
+    print("Begin eQTL analysis")
+
+    meGene = Matrix_eQTL_main(snps=theSnps, gene = geneSlice, 
+    	cvrt = covsGene, output_file_name.cis =  ".ctxt" ,
+    	pvOutputThreshold.cis = 1,  pvOutputThreshold=0,
+    	snpspos = snpspos, genepos = posGene, 
+    	useModel = modelLINEAR,	cisDist=5e5,
+    	pvalue.hist = 100,min.pv.by.genesnp = TRUE)	
+
+    meExon = Matrix_eQTL_main(snps=theSnps, gene = exonSlice, 
+    	cvrt = covsExon, output_file_name.cis =  ".ctxt" ,
+    	pvOutputThreshold.cis = 1,  pvOutputThreshold=0,
+    	snpspos = snpspos, genepos = posExon, 
+    	useModel = modelLINEAR,	cisDist=5e5,
+    	pvalue.hist = 100,min.pv.by.genesnp = TRUE)	
+
+    meJxn = Matrix_eQTL_main(snps=theSnps, gene = jxnSlice, 
+    	cvrt = covsJxn, output_file_name.cis =  ".ctxt" ,
+    	pvOutputThreshold.cis = 1,  pvOutputThreshold=0,
+    	snpspos = snpspos, genepos = posJxn, 
+    	useModel = modelLINEAR,	cisDist=5e5,
+    	pvalue.hist = 100,min.pv.by.genesnp = TRUE)	
 	
-meTx = Matrix_eQTL_main(snps=theSnps, gene = txSlice, 
-	cvrt = covsTx, output_file_name.cis =  ".ctxt" ,
-	pvOutputThreshold.cis = 1,  pvOutputThreshold=0,
-	snpspos = snpspos, genepos = posTx, 
-	useModel = modelLINEAR,	cisDist=5e5,
-	pvalue.hist = 100,min.pv.by.genesnp = TRUE)	
+    meTx = Matrix_eQTL_main(snps=theSnps, gene = txSlice, 
+    	cvrt = covsTx, output_file_name.cis =  ".ctxt" ,
+    	pvOutputThreshold.cis = 1,  pvOutputThreshold=0,
+    	snpspos = snpspos, genepos = posTx, 
+    	useModel = modelLINEAR,	cisDist=5e5,
+    	pvalue.hist = 100,min.pv.by.genesnp = TRUE)	
 
-save(meGene, meExon, meJxn, meTx,
-	file="eqtl_tables/matrixEqtl_output_dlpfc_4features.rda")
+    save(meGene, meExon, meJxn, meTx,
+    	file="eqtl_tables/matrixEqtl_output_dlpfc_4features.rda")
+} else {
+    rm(keepInd)
+    message(paste(Sys.time(), 'loading eqtl_tables/matrixEqtl_output_dlpfc_4features.rda'))
+    load("eqtl_tables/matrixEqtl_output_dlpfc_4features.rda", verbose = TRUE)
+}
 
-	
 ######################
 ###### annotate ######
-
-## Resume manually after memory crash...
-if(FALSE) {
-    load("/dcl01/lieber/ajaffe/lab/brainseq_phase2/bsp1/data/bsp1_gene.Rdata", verbose = TRUE)
-    load("/dcl01/lieber/ajaffe/lab/brainseq_phase2/bsp1/data/bsp1_exon.Rdata", verbose = TRUE)
-    load("/dcl01/lieber/ajaffe/lab/brainseq_phase2/bsp1/data/bsp1_jxn.Rdata", verbose = TRUE)
-    load("/dcl01/lieber/ajaffe/lab/brainseq_phase2/bsp1/data/bsp1_tx.Rdata", verbose = TRUE)
-
-    keepInd = which(colData(bsp1_gene)$Age > 13 & colData(bsp1_gene)$Region == "DLPFC")
-    rse_gene = bsp1_gene[,keepInd]
-    rse_exon = bsp1_exon[,keepInd]
-    rse_jxn = bsp1_jxn[,keepInd]
-    rse_tx = bsp1_tx[,keepInd]
-    rm(bsp1_gene, bsp1_exon, bsp1_jxn, bsp1_tx, keepInd)
-
-    load("eqtl_tables/matrixEqtl_output_dlpfc_4features.rda", verbose = TRUE)
-    
-}
 
 # extract
 geneEqtl = meGene$cis$eqtls
