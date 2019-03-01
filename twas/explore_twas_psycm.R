03719ba84677717d5f7f96f13ab3673eab25ba92
lcolladotor <lcollado@jhsph.edu>
2019-03-01 10:00:42 -0500

    Switch to purrr code

--- twas/explore_twas_psycm.R
+++ twas/explore_twas_psycm.R
@@ -2,36 +2,37 @@
 
 library('tibble')
 library('sessioninfo')
-# library('purrr')
+library('purrr')
 # library('dplyr')
 
 load('rda/twas_exp.Rdata', verbose = TRUE)
 
 ## Andrew's exploration code that focuses on the 'all' part
-# tt = as.data.frame(twas_exp$all)
 tt <- twas_exp$all
 ## Drop TWAS NA p-values
 tt <- tt[!is.na(tt$TWAS.P), ]
-tt = tt[which(tt$type == "psycm"),]
+## Focus on CLOZUK+PGC2 (psycm) GWAS
+tt <- tt[which(tt$type == "psycm"),]
 
-ttHip = tt[tt$region == "HIPPO",]
-ttDLPFC = tt[tt$region == "DLPFC",]
+## Compute FDR by region across all 4 features
+tt <- map_dfr(split(tt, tt$region), function(reg) {
+    reg$TWAS.FDR <- p.adjust(reg$TWAS.P, 'fdr')
+    reg <- reg[order(reg$TWAS.P), ]
+    return(reg)
+})
+print(tt, width = 200)
 
-ttDLPFC$FDR = p.adjust(ttDLPFC$TWAS.P,"fdr")
-ttHip$FDR = p.adjust(ttHip$TWAS.P,"fdr")
-
-ttSig_hip = ttHip[which(ttHip$FDR < 0.05),]
-ttSig_hip = ttSig_hip[order(ttSig_hip$TWAS.P),]
-ttSig_dlp = ttDLPFC[which(ttDLPFC$FDR < 0.05),]
-ttSig_dlp = ttSig_dlp[order(ttSig_dlp$TWAS.P),]
-
-dim(ttSig_dlp)
+ttSig <- map(split(tt, tt$region), ~ .x[.x$TWAS.FDR < 0.05, ])
+map(ttSig, dim)
+# $DLPFC
 # [1] 5762   27
-dim(ttSig_hip)
+#
+# $HIPPO
 # [1] 4090   27
 
-length(unique(ttSig_dlp$geneid)) #  1519
-length(unique(ttSig_hip$geneid)) #  1256
+map_int(ttSig, ~ length(unique(.x$geneid)))
+# DLPFC HIPPO
+#  1519  1256
 
 ## Continue
 
