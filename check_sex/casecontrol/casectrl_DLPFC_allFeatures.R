49e3df7b775dbad1cf64195ac19def411646932a
lcolladotor <lcollado@jhsph.edu>
2019-03-07 14:09:55 -0500

    Run SCZD case vs control DE for HIPPO and DLPFC looking for DE signal by sex adjusting for the same covariates as in the full model

--- check_sex/casecontrol/casectrl_DLPFC_allFeatures.R
+++ check_sex/casecontrol/casectrl_DLPFC_allFeatures.R
@@ -1,11 +1,12 @@
-# Adapted from casectrl_DLPFC.R and
-# https://github.com/LieberInstitute/brainseq_phase2/blob/master/caseControl_analysis_DLPFC.R
+# Adapted from /dcl01/ajaffe/data/lab/qsva_brain/brainseq_phase2_qsv/casectrl_DLPFC_allFeatures.R 
 library(jaffelab)
 library(SummarizedExperiment)
 library(limma)
 library(edgeR)
 library('devtools')
 
+dir.create('rdas', showWarnings = FALSE)
+dir.create('pdf', showWarnings = FALSE)
 
 ### Run with Gold samples using the qSVs made without the age>17 samples
 ## and without the HIPPO Gold samples (qsv are DLPFC specific)
@@ -31,12 +32,41 @@ rse_tx <- rse_tx[, keepIndex]
 mod <- mod[keepIndex, ]
 modQsva <- modQsva[keepIndex, ]
 
+## Make sex the second coefficient
+check_sex <- function(mat) {
+    i_sex <- grep('sex', tolower(colnames(mat)))
+    i_rest <- seq_len(ncol(mat))
+    i_rest <- i_rest[-c(1, i_sex)]
+    mat[, c(1, i_sex, i_rest)]
+}
+mod <- check_sex(mod)
+modQsva <- check_sex(modQsva)
+colnames(mod)
+#  [1] "(Intercept)"       "SexM"              "DxSchizo"
+#  [4] "Age"               "mitoRate"          "rRNA_rate"
+#  [7] "totalAssignedGene" "RIN"               "snpPC1"
+# [10] "snpPC2"            "snpPC3"            "snpPC4"
+# [13] "snpPC5"
+colnames(modQsva)
+#  [1] "(Intercept)"       "SexM"              "DxSchizo"
+#  [4] "Age"               "mitoRate"          "rRNA_rate"
+#  [7] "totalAssignedGene" "RIN"               "snpPC1"
+# [10] "snpPC2"            "snpPC3"            "snpPC4"
+# [13] "snpPC5"            "PC1"               "PC2"
+# [16] "PC3"               "PC4"               "PC5"
+# [19] "PC6"               "PC7"               "PC8"
+# [22] "PC9"               "PC10"              "PC11"
+# [25] "PC12"              "PC13"              "PC14"
+# [28] "PC15"
+
 ##### GENE ######
 dge = DGEList(counts = assays(rse_gene)$counts,
 	genes = rowData(rse_gene))
 #calculate library-size adjustment
 dge = calcNormFactors(dge)
-vGene = voom(dge,modQsva, plot=FALSE)
+pdf('pdf/dlpfc_voom_qsva_noHGoldQSV_matchDLPFC_gene.pdf', useDingbats = FALSE)
+vGene = voom(dge,modQsva, plot=TRUE)
+dev.off()
 fitGene = lmFit(vGene)
 eBGene = eBayes(fitGene)
 sigGene = topTable(eBGene,coef=2,
@@ -89,76 +119,96 @@ proc.time()
 options(width = 120)
 session_info()
 
-# Session info ----------------------------------------------------------------------------------------------------------
+# ─ Session info ───────────────────────────────────────────────────────────────────────────────────────────────────────
 #  setting  value
-#  version  R version 3.4.3 Patched (2018-01-20 r74142)
+#  version  R version 3.5.1 Patched (2018-10-29 r75535)
+#  os       Red Hat Enterprise Linux Server release 6.9 (Santiago)
 #  system   x86_64, linux-gnu
 #  ui       X11
 #  language (EN)
 #  collate  en_US.UTF-8
+#  ctype    en_US.UTF-8
 #  tz       US/Eastern
-#  date     2018-04-26
+#  date     2019-03-07
 #
-# Packages --------------------------------------------------------------------------------------------------------------
-#  package              * version   date       source
-#  base                 * 3.4.3     2018-01-20 local
-#  Biobase              * 2.38.0    2017-11-07 Bioconductor
-#  BiocGenerics         * 0.24.0    2017-11-29 Bioconductor
-#  bitops                 1.0-6     2013-08-17 CRAN (R 3.4.1)
-#  colorout             * 1.2-0     2018-02-19 Github (jalvesaq/colorout@2f01173)
-#  colorspace             1.3-2     2016-12-14 CRAN (R 3.4.1)
-#  compiler               3.4.3     2018-01-20 local
-#  datasets             * 3.4.3     2018-01-20 local
-#  DelayedArray         * 0.4.1     2017-11-07 Bioconductor
-#  devtools             * 1.13.5    2018-02-18 CRAN (R 3.4.3)
-#  digest                 0.6.15    2018-01-28 cran (@0.6.15)
-#  edgeR                * 3.20.9    2018-04-18 Bioconductor
-#  GenomeInfoDb         * 1.14.0    2017-11-29 Bioconductor
-#  GenomeInfoDbData       1.0.0     2018-01-09 Bioconductor
-#  GenomicRanges        * 1.30.3    2018-04-18 Bioconductor
-#  ggplot2                2.2.1     2016-12-30 CRAN (R 3.4.1)
-#  graphics             * 3.4.3     2018-01-20 local
-#  grDevices            * 3.4.3     2018-01-20 local
-#  grid                   3.4.3     2018-01-20 local
-#  gtable                 0.2.0     2016-02-26 CRAN (R 3.4.1)
-#  htmltools              0.3.6     2017-04-28 CRAN (R 3.4.1)
-#  htmlwidgets            1.2       2018-04-19 CRAN (R 3.4.3)
-#  httpuv                 1.3.6.2   2018-03-02 CRAN (R 3.4.3)
-#  IRanges              * 2.12.0    2017-11-29 Bioconductor
-#  jaffelab             * 0.99.20   2018-04-19 Github (LieberInstitute/jaffelab@04c470a)
-#  later                  0.7.1     2018-03-07 CRAN (R 3.4.3)
-#  lattice                0.20-35   2017-03-25 CRAN (R 3.4.3)
-#  lazyeval               0.2.1     2017-10-29 CRAN (R 3.4.2)
-#  limma                * 3.34.9    2018-04-18 Bioconductor
-#  locfit                 1.5-9.1   2013-04-20 CRAN (R 3.4.1)
-#  Matrix                 1.2-12    2017-11-30 CRAN (R 3.4.3)
-#  matrixStats          * 0.53.1    2018-02-11 CRAN (R 3.4.3)
-#  memoise                1.1.0     2017-04-21 CRAN (R 3.4.1)
-#  methods              * 3.4.3     2018-01-20 local
-#  mime                   0.5       2016-07-07 CRAN (R 3.4.1)
-#  munsell                0.4.3     2016-02-13 CRAN (R 3.4.1)
-#  parallel             * 3.4.3     2018-01-20 local
-#  pillar                 1.2.1     2018-02-27 CRAN (R 3.4.3)
-#  plyr                   1.8.4     2016-06-08 CRAN (R 3.4.1)
-#  png                    0.1-7     2013-12-03 CRAN (R 3.4.1)
-#  rafalib              * 1.0.0     2015-08-09 CRAN (R 3.4.1)
-#  RColorBrewer           1.1-2     2014-12-07 CRAN (R 3.4.1)
-#  Rcpp                   0.12.16   2018-03-13 CRAN (R 3.4.3)
-#  RCurl                  1.95-4.10 2018-01-04 CRAN (R 3.4.2)
-#  rlang                  0.2.0     2018-02-20 CRAN (R 3.4.3)
-#  rmote                * 0.3.4     2018-02-16 deltarho (R 3.4.3)
-#  S4Vectors            * 0.16.0    2017-11-29 Bioconductor
-#  scales                 0.5.0     2017-08-24 CRAN (R 3.4.1)
-#  segmented              0.5-3.0   2017-11-30 CRAN (R 3.4.2)
-#  servr                  0.9       2018-03-25 CRAN (R 3.4.3)
-#  stats                * 3.4.3     2018-01-20 local
-#  stats4               * 3.4.3     2018-01-20 local
-#  SummarizedExperiment * 1.8.1     2018-01-09 Bioconductor
-#  tibble                 1.4.2     2018-01-22 CRAN (R 3.4.3)
-#  tools                  3.4.3     2018-01-20 local
-#  utils                * 3.4.3     2018-01-20 local
-#  withr                  2.1.2     2018-03-15 CRAN (R 3.4.3)
-#  xfun                   0.1       2018-01-22 CRAN (R 3.4.3)
-#  XVector                0.18.0    2017-11-29 Bioconductor
-#  zlibbioc               1.24.0    2017-11-07 Bioconductor
+# ─ Packages ───────────────────────────────────────────────────────────────────────────────────────────────────────────
+#  package              * version   date       lib source
+#  assertthat             0.2.0     2017-04-11 [2] CRAN (R 3.5.0)
+#  backports              1.1.3     2018-12-14 [2] CRAN (R 3.5.1)
+#  bindr                  0.1.1     2018-03-13 [1] CRAN (R 3.5.0)
+#  bindrcpp               0.2.2     2018-03-29 [1] CRAN (R 3.5.0)
+#  Biobase              * 2.42.0    2018-10-30 [2] Bioconductor
+#  BiocGenerics         * 0.28.0    2018-10-30 [1] Bioconductor
+#  BiocParallel         * 1.16.5    2019-01-04 [1] Bioconductor
+#  bitops                 1.0-6     2013-08-17 [2] CRAN (R 3.5.0)
+#  callr                  3.1.1     2018-12-21 [2] CRAN (R 3.5.1)
+#  cli                    1.0.1     2018-09-25 [1] CRAN (R 3.5.1)
+#  colorout             * 1.2-0     2018-05-02 [1] Github (jalvesaq/colorout@c42088d)
+#  colorspace             1.4-0     2019-01-13 [2] CRAN (R 3.5.1)
+#  crayon                 1.3.4     2017-09-16 [1] CRAN (R 3.5.0)
+#  DelayedArray         * 0.8.0     2018-10-30 [2] Bioconductor
+#  desc                   1.2.0     2018-05-01 [2] CRAN (R 3.5.1)
+#  devtools             * 2.0.1     2018-10-26 [1] CRAN (R 3.5.1)
+#  digest                 0.6.18    2018-10-10 [1] CRAN (R 3.5.1)
+#  dplyr                  0.7.8     2018-11-10 [1] CRAN (R 3.5.1)
+#  edgeR                * 3.24.3    2019-01-02 [1] Bioconductor
+#  fs                     1.2.6     2018-08-23 [2] CRAN (R 3.5.1)
+#  GenomeInfoDb         * 1.18.1    2018-11-12 [1] Bioconductor
+#  GenomeInfoDbData       1.2.0     2018-11-02 [2] Bioconductor
+#  GenomicRanges        * 1.34.0    2018-10-30 [1] Bioconductor
+#  ggplot2                3.1.0     2018-10-25 [1] CRAN (R 3.5.1)
+#  glue                   1.3.0     2018-07-17 [1] CRAN (R 3.5.1)
+#  gtable                 0.2.0     2016-02-26 [2] CRAN (R 3.5.0)
+#  htmltools              0.3.6     2017-04-28 [2] CRAN (R 3.5.0)
+#  htmlwidgets            1.3       2018-09-30 [1] CRAN (R 3.5.1)
+#  httpuv                 1.4.5.1   2018-12-18 [2] CRAN (R 3.5.1)
+#  IRanges              * 2.16.0    2018-10-30 [1] Bioconductor
+#  jaffelab             * 0.99.21   2018-05-03 [1] Github (LieberInstitute/jaffelab@7ed0ab7)
+#  later                  0.7.5     2018-09-18 [2] CRAN (R 3.5.1)
+#  lattice                0.20-38   2018-11-04 [3] CRAN (R 3.5.1)
+#  lazyeval               0.2.1     2017-10-29 [2] CRAN (R 3.5.0)
+#  limma                * 3.38.3    2018-12-02 [1] Bioconductor
+#  locfit                 1.5-9.1   2013-04-20 [2] CRAN (R 3.5.0)
+#  magrittr               1.5       2014-11-22 [1] CRAN (R 3.5.0)
+#  Matrix                 1.2-15    2018-11-01 [3] CRAN (R 3.5.1)
+#  matrixStats          * 0.54.0    2018-07-23 [1] CRAN (R 3.5.1)
+#  memoise                1.1.0     2017-04-21 [2] CRAN (R 3.5.0)
+#  munsell                0.5.0     2018-06-12 [2] CRAN (R 3.5.0)
+#  pillar                 1.3.1     2018-12-15 [1] CRAN (R 3.5.1)
+#  pkgbuild               1.0.2     2018-10-16 [2] CRAN (R 3.5.1)
+#  pkgconfig              2.0.2     2018-08-16 [1] CRAN (R 3.5.1)
+#  pkgload                1.0.2     2018-10-29 [2] CRAN (R 3.5.1)
+#  plyr                   1.8.4     2016-06-08 [2] CRAN (R 3.5.0)
+#  png                    0.1-7     2013-12-03 [2] CRAN (R 3.5.0)
+#  prettyunits            1.0.2     2015-07-13 [1] CRAN (R 3.5.0)
+#  processx               3.2.1     2018-12-05 [1] CRAN (R 3.5.1)
+#  promises               1.0.1     2018-04-13 [2] CRAN (R 3.5.0)
+#  ps                     1.3.0     2018-12-21 [2] CRAN (R 3.5.1)
+#  purrr                  0.2.5     2018-05-29 [2] CRAN (R 3.5.0)
+#  R6                     2.3.0     2018-10-04 [2] CRAN (R 3.5.1)
+#  rafalib              * 1.0.0     2015-08-09 [1] CRAN (R 3.5.0)
+#  RColorBrewer           1.1-2     2014-12-07 [2] CRAN (R 3.5.0)
+#  Rcpp                   1.0.0     2018-11-07 [1] CRAN (R 3.5.1)
+#  RCurl                  1.95-4.11 2018-07-15 [2] CRAN (R 3.5.1)
+#  remotes                2.0.2     2018-10-30 [1] CRAN (R 3.5.1)
+#  rlang                  0.3.1     2019-01-08 [1] CRAN (R 3.5.1)
+#  rmote                * 0.3.4     2018-05-02 [1] deltarho (R 3.5.0)
+#  rprojroot              1.3-2     2018-01-03 [2] CRAN (R 3.5.0)
+#  S4Vectors            * 0.20.1    2018-11-09 [1] Bioconductor
+#  scales                 1.0.0     2018-08-09 [2] CRAN (R 3.5.1)
+#  segmented              0.5-3.0   2017-11-30 [2] CRAN (R 3.5.0)
+#  servr                  0.11      2018-10-23 [1] CRAN (R 3.5.1)
+#  sessioninfo            1.1.1     2018-11-05 [1] CRAN (R 3.5.1)
+#  SummarizedExperiment * 1.12.0    2018-10-30 [1] Bioconductor
+#  testthat               2.0.1     2018-10-13 [1] CRAN (R 3.5.1)
+#  tibble                 2.0.1     2019-01-12 [1] CRAN (R 3.5.1)
+#  tidyselect             0.2.5     2018-10-11 [2] CRAN (R 3.5.1)
+#  usethis              * 1.4.0     2018-08-14 [2] CRAN (R 3.5.1)
+#  withr                  2.1.2     2018-03-15 [2] CRAN (R 3.5.0)
+#  xfun                   0.4       2018-10-23 [1] CRAN (R 3.5.1)
+#  XVector                0.22.0    2018-10-30 [1] Bioconductor
+#  zlibbioc               1.28.0    2018-10-30 [2] Bioconductor
 #
+# [1] /users/lcollado/R/x86_64-pc-linux-gnu-library/3.5.x
+# [2] /jhpce/shared/jhpce/core/conda/miniconda-3/envs/svnR-3.5.x/R/3.5.x/lib64/R/site-library
+# [3] /jhpce/shared/jhpce/core/conda/miniconda-3/envs/svnR-3.5.x/R/3.5.x/lib64/R/library
